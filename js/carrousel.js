569b908cbc2047cf33514bae92816c00f9854af0
Ronilson2002 <ronyronilson2002@gmail.com>
2023-01-11 07:02:47 +0100

    Alterações Radicais acoonteceram

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,29 +1,178 @@
 // Código em Ascenção
 
-/* var slideIndex = 1;
+
+document.getElementsByClassName("buscar")[0].onclick = function() {mostrarSlides()};
+
+
+var slideIndice = 0;
+function mostrarSlides() {
+  var nums = ["screen.png", "blackson@2x.png", "reverse2@2x.png", "cluedo@2x.png", "nani2@2x.png", "watashi@2x.png", "pernalta@2x.png" ,"kimyona@2x.png"]
+  var path = "";
+  var slider = document.getElementById("inicio")
+  
+  if (slideIndice >= nums.length) {
+    slideIndice = 0;
+  }
+  path = "url('../img/covers/" + nums[slideIndice] + "')"
+  // alert(slideIndice)
+  // alert(path)
+  slider.style.background = path + "center top repeat-x";
+  slider.style.backgroundSize = "cover";
+  slideIndice++;
+  setTimeout(mostrarSlides, 3000);
+}
+
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btnclicke");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("ativo");
+    current[0].className = current[0].className.replace(" ativo", "");
+    this.className += " ativo";
+  });
+}
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("myBtnContainer");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }}
+
+  var animes = ["Blackson", "Kymiona Sekai", "Pernalta", "Nervous", "Cleudo", "Reverse Reality", "Reverse Reality2", "Nani Osaka", "Nani Osaka2", "Watashi No Kagaku", "Watashi No Kagaku2"]
+  function autocomplete(inp, arr) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function(e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false;}
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+          /*check if the item starts with the same letters as the text field value:*/
+          if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+            /*create a DIV element for each matching element:*/
+            b = document.createElement("DIV");
+            /*make the matching letters bold:*/
+            b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+            b.innerHTML += arr[i].substr(val.length);
+            /*insert a input field that will hold the current array item's value:*/
+            b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+            /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function(e) {
+                /*insert the value for the autocomplete text field:*/
+                inp.value = this.getElementsByTagName("input")[0].value;
+                /*close the list of autocompleted values,
+                (or any other open lists of autocompleted values:*/
+                closeAllLists();
+            });
+            a.appendChild(b);
+          }
+        }
+    });
+
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function(e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          /*If the arrow DOWN key is pressed,
+          increase the currentFocus variable:*/
+          currentFocus++;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 38) { //up
+          /*If the arrow UP key is pressed,
+          decrease the currentFocus variable:*/
+          currentFocus--;
+          /*and and make the current item more visible:*/
+          addActive(x);
+        } else if (e.keyCode == 13) {
+          /*If the ENTER key is pressed, prevent the form from being submitted,*/
+          e.preventDefault();
+          if (currentFocus > -1) {
+            /*and simulate a click on the "active" item:*/
+            if (x) x[currentFocus].click();
+          }
+        }
+    });
+    function addActive(x) {
+      /*a function to classify an item as "active":*/
+      if (!x) return false;
+      /*start by removing the "active" class on all items:*/
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = (x.length - 1);
+      /*add class "autocomplete-active":*/
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+    function removeActive(x) {
+      /*a function to remove the "active" class from all autocomplete items:*/
+      for (var i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+    function closeAllLists(elmnt) {
+      /*close all autocomplete lists in the document,
+      except the one passed as an argument:*/
+      var x = document.getElementsByClassName("autocomplete-items");
+      for (var i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+  }
+
+autocomplete(document.getElementById("buscar-texto"), animes);
+
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
-function plusSlides() {
-  showSlides();
+// Next/previous controls
+function plusSlides(n) {
+showSlides(slideIndex += n);
 }
 
+// Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+showSlides(slideIndex = n);
 }
 
-function showSlides() {
-  var i;
-var slides = document.getElementById("inicio")
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[0].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides.style.background = "url('../img/4.png')  center bottom repeat-x";
-  slides.style.backgroundSize = "cover";
-  dots[slideIndex-1].className += " active";
-} */
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("meuSlides");
+var dots = document.getElementsByClassName("demo");
+var captionText = document.getElementById("caption");
+if (n > slides.length) {slideIndex = 1}
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+  slides[i].style.display = "none";
+}
+for (i = 0; i < dots.length; i++) {
+  dots[i].className = dots[i].className.replace(" ativado", "");
+}
+slides[slideIndex-1].style.display = "block";
+dots[slideIndex-1].className += " ativado";
+captionText.innerHTML = dots[slideIndex-1].alt;
+}
---
+++ js/consum.js
@@ -0,0 +1,80 @@
+// Ler arquivo json
+
+
+document.getElementsByClassName("btnModal")[0].onclick = function (){Carregar(0);}
+document.getElementsByClassName("linkModal")[0].onclick = function (){Carregar(0);}
+
+document.getElementsByClassName("btnModal")[1].onclick = function (){Carregar(1);}
+document.getElementsByClassName("linkModal")[1].onclick = function (){Carregar(1);}
+
+document.getElementsByClassName("btnModal")[2].onclick = function (){Carregar(2);}
+document.getElementsByClassName("linkModal")[2].onclick = function (){Carregar(2);}
+
+document.getElementsByClassName("btnModal")[3].onclick = function (){Carregar(3);}
+document.getElementsByClassName("linkModal")[3].onclick = function (){Carregar(3);}
+
+document.getElementsByClassName("btnModal")[4].onclick = function (){Carregar(4);}
+document.getElementsByClassName("linkModal")[4].onclick = function (){Carregar(4);}
+
+document.getElementsByClassName("btnModal")[5].onclick = function (){Carregar(5);}
+document.getElementsByClassName("linkModal")[5].onclick = function (){Carregar(5);}
+
+document.getElementsByClassName("btnModal")[6].onclick = function (){Carregar(6);}
+document.getElementsByClassName("linkModal")[6].onclick = function (){Carregar(6);}
+
+document.getElementsByClassName("btnModal")[7].onclick = function (){Carregar(7);}
+document.getElementsByClassName("linkModal")[7].onclick = function (){Carregar(7);}
+
+document.getElementsByClassName("btnModal")[8].onclick = function (){Carregar(8);}
+document.getElementsByClassName("linkModal")[8].onclick = function (){Carregar(8);}
+
+document.getElementsByClassName("btnModal")[9].onclick = function (){Carregar(9);}
+document.getElementsByClassName("linkModal")[9].onclick = function (){Carregar(9);}
+
+document.getElementsByClassName("btnModal")[10].onclick = function (){Carregar(10);}
+document.getElementsByClassName("linkModal")[10].onclick = function (){Carregar(10);}
+
+
+document.getElementById("rony").onclick = function(){Abrir(); }
+
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function(){
+        var div = this.parentElement;
+        div.style.opacity = "0";
+        setTimeout(function(){ div.style.display = "none"; }, 600);
+    }
+}
+
+function Abrir() {
+    document.getElementsByClassName("alerta")[0].style.display = "block";
+    
+}
+function Carregar(ind) {
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+
+
+        var myObj = JSON.parse(this.responseText);
+        document.getElementById("modalTitle").innerHTML = myObj.cartoons[ind].modalTitle        
+        document.getElementById("tamanhoArquivo").innerHTML = myObj.cartoons[ind].tamanhoArquivo        
+        document.getElementById("roteirista").innerHTML = myObj.cartoons[ind].roteirista        
+        document.getElementById("genero").innerHTML = myObj.cartoons[ind].genero        
+        document.getElementById("paginas").innerHTML = myObj.cartoons[ind].paginas        
+        document.getElementById("paginas").innerHTML = myObj.cartoons[ind].desenhista        
+        document.getElementById("editora").innerHTML = myObj.cartoons[ind].editora        
+        document.getElementById("recomendacao").innerHTML = myObj.cartoons[ind].recomendacao        
+        document.getElementById("colorista").innerHTML = myObj.cartoons[ind].colorista        
+        document.getElementById("avaliacao").innerHTML = myObj.cartoons[ind].avaliacao        
+        document.getElementById("dataPub").innerHTML = myObj.cartoons[ind].dataPub        
+        document.getElementById("sinopse").innerHTML = myObj.cartoons[ind].sinopse        
+        
+
+    }
+        };
+    xmlhttp.open("GET", "../dados.json", true);
+    xmlhttp.send();
+}
+
+
